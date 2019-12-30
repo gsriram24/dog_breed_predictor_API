a5ddcda03dd8a76fff62bf0725160c11e53150c7
G Sriram <sgcuber24@gmail.com>
2019-12-30 22:31:52 +0530

    Image path change

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ with open('dog_names.txt', 'r') as f:
 
 
 def extract_VGG19(tensor):
-
     return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
 
@@ -23,11 +22,6 @@ def path_to_tensor(img_path):
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     return np.expand_dims(x, axis=0)
-
-
-def paths_to_tensor(img_paths):
-    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
-    return np.vstack(list_of_tensors)
 
 
 def VGG19_predict_breed(img_path):
@@ -42,16 +36,10 @@ VGG19_model = load_model('saved_models/VGG19_model.h5')
 @app.route('/', methods=['POST'])
 def api_id():
     file = request.files['file']
-    filename = file.filename
-
-    file_directory = os.path.join('images/', filename)
-    file.save(file_directory)
     results = {}
-    results['data'] = VGG19_predict_breed(file_directory)
-    os.remove(file_directory)
+    results['data'] = VGG19_predict_breed(file)
     return jsonify(results)
 
 
 if __name__ == '__main__':
-    os.makedirs('images/')
     app.run(threaded=False, port=80)
